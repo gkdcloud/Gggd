95cb94d07b287753f5ac3a7aa03d103c031494be
gao1354184430 <50170219+gao1354184430@users.noreply.github.com>
2021-09-11 10:32:01 +0800

    Update update.sh

--- test/test5/update.sh
+++ test/test5/update.sh
@@ -2,30 +2,21 @@
 #远端文件储存地址
 #升级内容开始
 function update() {
-		echo "[Unit]
-Description=The nginx HTTP and reverse proxy server
-After=syslog.target network.target remote-fs.target nss-lookup.target
- 
-[Service]
-Type=forking
-PIDFile=/usr/local/nginx/logs/nginx.pid
-ExecStartPre=/usr/local/nginx/sbin/nginx -t
-ExecStart=/usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx.conf
-ExecReload=/usr/local/nginx/sbin/nginx -s reload
-ExecStop=/bin/kill -s QUIT $MAINPID
-PrivateTmp=true
- 
-[Install]
-WantedBy=multi-user.target" >/lib/systemd/system/nginx.service
-systemctl daemon-reload
+#更新cnsync
 rm -rf /usr/bin/cnsync ; wget https://ghproxy.net/https://raw.githubusercontent.com/gao1354184430/Gggd/master/test/test5/cnsync -O /usr/bin/cnsync ; chmod +x /usr/bin/cnsync
+#更新nginx
+cndownload="https://ghproxy.net/https://raw.githubusercontent.com/gao1354184430/Gggd/master/test/test5"
+rm -rf /usr/local/nginx/conf/nginx.conf
+wget -N -P /usr/local/nginx/conf/ $cndownload/download/nginx.conf
+systemctl reload nginx
+systemctl status nginx
+echo "请检查状态确保无异常"
 }
 function menu_server() {
   echo && echo -e "  临时用升级脚本
  ————————————
- 1.修复Tengine使其支持重载配置
- 2.更新cnsync：独立GKD配置文件，使用Nginx重载配置代替重启
- 3.更新cnsync：添加守护程序并增加TG通知
+ 1.增加nginx对四层(TCP/UDP)转发的支持
+ 2.更新cnsync：增加对ngin-tcp同步的支持
 ————————————
 " &&echo 
   echo
