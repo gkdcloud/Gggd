1a52b30e1d6903238d589267f80274e3cad27149
gao1354184430 <50170219+gao1354184430@users.noreply.github.com>
2021-09-20 22:02:41 +0800

    Update index.js

--- test/js/index.js
+++ test/js/index.js
@@ -5,5 +5,6 @@ var requestHandlers =require("./requestHandlers");
 var handle = {}
 handle["/"] = requestHandlers.hello;
 handle["/get"] = requestHandlers.get;
+handle["post"] = requestHandlers.read
 
 server.start(router.route, handle);
