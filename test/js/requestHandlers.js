4fe31ca7e0dd5eb255ae65161a0ab017bddd612b
gao1354184430 <50170219+gao1354184430@users.noreply.github.com>
2021-09-20 22:02:54 +0800

    Update requestHandlers.js

--- test/js/requestHandlers.js
+++ test/js/requestHandlers.js
@@ -76,22 +76,18 @@ function hello(query, response) {
     response.end();
 }
 
-function nginx_conf(query, response)
-{
-	var fs=require('fs')
-console.log('以下是nginx_conf文件');
-//异步读取mjnginx
+function read(query, response){
+    var fs=require('fs')
+console.log('程序开始运行');
 fs.readFile('/usr/local/nginx/mjnginx.conf',function(err,data){
     if(err){
         return console.log(err);
     }
     console.log(data.toString());
 });
-
-console.log('以上是nginx_conf文件');
-
-
+console.log('程序结束运行');
 }
 
 exports.get = get;
 exports.hello = hello;
+export.read = read
